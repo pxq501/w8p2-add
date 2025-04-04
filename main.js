d9751b1cbd6a0817371fe3d8e2206f8c444071c4
pxq501 <146098158+pxq501@users.noreply.github.com>
2025-04-04 14:12:59 +0100

    123
    123

---
+++ main.js
@@ -0,0 +1,16 @@
+async function getADog() {
+    const response = await fetch("https://random.dog/woof.json");
+    const data = await response.json();
+    console.log(data);
+    document.getElementById("img1").src = data.url;
+}
+
+document.getElementById("text").addEventListener("keyup", wordcount)
+
+function wordcount() {
+    text = document.getElementById("text").innerText;
+    const textArray = text.split(" ");
+    if(textArray.length ) {
+        getADog()
+    }
+}
